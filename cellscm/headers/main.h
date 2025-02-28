4f66c3fcef242319497082d647bfff50617673db
Zijian Qin <zq5820@orion.cs.princeton.edu>
2025-02-27 20:20:26 -0500

    update for cellscm

--- cellscm/headers/main.h
+++ cellscm/headers/main.h
@@ -2,7 +2,11 @@
 #define _MAIN_CELL_SCM_
 
 #include <iostream>
-#include<stdio.h>
+#include <memory>
+#include <string>
+#include <stdio.h>
+#include <libconfig.h>
+#include <unistd.h>
 
 #include "srsran/srsran.h"
 #include "srsran/phy/common/phy_common.h"
@@ -10,5 +14,34 @@
 #include "srsran/asn1/rrc/si.h"
 #include "srsran/asn1/rrc.h"
 #include "srsran/asn1/rrc_utils.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int mib_search(srsran_rf_t* rf, uint32_t nof_rx_channels, cell_search_cfg_t* config, int force_N_id_2, srsran_cell_t* cell, float* cfo);
+
+typedef struct {
+    srsran_ue_mib_t mib;
+    asn1::rrc::sib_type1_s sib1;
+    asn1::rrc::sib_type2_s sib2;
+
+    int bw;
+}cell_cfg_t;
+
+typedef struct {
+    /* data */
+}ue_db_t;
+
+typedef struct {
+    srsran_cell_t cell;
+    cell_cfg_t cell_cfg;
+    ue_db_t ue_db;
+}cells_t;
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
